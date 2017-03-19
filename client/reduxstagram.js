ff76b92a7b7211ec3d3e50b9ac9dc1a04e9396bb
TallesFaria <tallesbeginsc@gmail.com>
2017-03-18 22:38:58 -0300

    I doenst test anything! Need to catch this up

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -8,8 +8,23 @@ import PhotoGrid from './components/PhotoGrid'
 import { Router, Route, IndexRoute, browserHistory} from 'react-router'
 import { Provider } from 'react-redux'
 import store, { history } from './store'
+import Raven from 'raven-js'
+import { sentry_url, logException } from './data/config'
 
+Raven.config(sentry_url, {
+    tags: {
+        git_commit: '', 
+        user_level:'editor'
+    }
+}).install()
 
+logException(new Error('download failed!'), {
+    email: 'teste@google.com'
+})
+
+Raven.captureMessage('Something went wrong!')
+
+Raven.showReportDialog()
 const router = (
     <Provider store={store}>
         <Router history={history}>
